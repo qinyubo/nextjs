9779c896310cade5d75dd7b17f0754b008b33c47
Bobo Q <yubo@Yubos-MacBook-Pro.local>
2021-07-20 12:21:24 -0400

    Init change

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h1>
+          Hello Bobo
+          </h1>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
